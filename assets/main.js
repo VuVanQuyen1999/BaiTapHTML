def0e4db3d7d294e2e65e877384d86d23d3a9ef3
VuVanQuyen1999 <VuVanQuyen07081999@gmail.com>
2022-05-25 16:22:46 +0700

    bai6

--- assets/main.js
+++ assets/main.js
@@ -7,3 +7,12 @@ document.getElementById('result').innerHTML = 'kết quả:' + ' ' + str.search(
 const repeat = str.match(/à/g)
 document.getElementById('repeat').innerHTML = 'kết quả trả về 1 mảng có các phần từ là chuỗi được lặp lại từ mảng gốc: => số lần lặp lại = repeat.length =' + ' ' + 'Số lần lặp lại của chuỗi "à" =' + ' ' + repeat.length
 console.log(repeat.length)
+
+
+// Bài 6
+
+function isVietnamesePhoneNumberValid(number) {
+    return /(((\+|)84)|0)(3|5|7|8|9)+([0-9]{8})\b/.test(number);
+}
+
+console.log(isVietnamesePhoneNumberValid(+84374762336))
